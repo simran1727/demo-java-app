973be3842433f89f8b4c3eb8fa1fc8a8d23e0338
Aerika <simrans01727@gmail.com>
2025-11-24 17:50:05 +0530

    Update App.java message for version 2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Aerika’s Jenkins build!");
+        System.out.println("Hello from Aerika's updated Java app – version 2!");
     }
 }
